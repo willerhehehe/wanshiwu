2c026a50b03097072ba07666d2a0bc8e5579360c
willerhehehe <32084498+willerhehehe@users.noreply.github.com>
2019-01-03 17:43:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ HOSTNAME = '127.0.0.1'
 PORT ='3306'
 DATABASE = 'wanshiwu_demo'
 USERNAME = 'root'
-PASSWORD = 'heyaoxu555'
+PASSWORD = ''
 DB_URI = "mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8".format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
